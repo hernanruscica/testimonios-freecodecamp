0294de5c5638fd66a3492854309150410ad5d514
hernanruscica <cesarhernanruscica@gmail.com>
2022-03-21 12:59:25 -0300

    testimonios de freecodecamp terminado, faltan los retos

--- src/componentes/Testimonios.jsx
+++ src/componentes/Testimonios.jsx
@@ -1,19 +1,17 @@
 import React from 'react';
 import '../hojas-de-estilo/Testimonios.css'; 
-function Testimonio() {
+function Testimonio(props) {
   return (
     <div className='contenedor-testimonio'>
       <img 
         className = 'imagen-testimonio' 
-        src ={require('../imagenes/testimonio-emma.png')}
+        src ={require(`../imagenes/testimonio-${props.imagen}.png`)}
         alt = 'Foto de Emma'
       />
       <div className = 'contenedor-texto-testimonio'>
-        <p className='nombre-testimonio'>Emma Bostian en Suecia</p>
-        <p className='cargo-testimonio'>Ingeniera en software en spotify</p>
-        <p className='texto-testimonio'>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore optio hic, necessitatibus dignissimos tempore, eaque modi quasi, quae qui delectus harum itaque beatae praesentium quaerat blanditiis culpa explicabo velit ducimus.
-        </p>
+        <p className='nombre-testimonio'><strong>{props.nombre}</strong> en {props.pais}</p>
+        <p className='cargo-testimonio'><strong>{props.cargo}</strong> en {props.empresa}</p>
+        <p className='texto-testimonio'>"{props.testimonio}"</p>
       </div>
     </div>
   );
